22f4ec4756e82aaef0e1b79f0f019fb2a89594de
techgeek04 <155185231+techgeek04@users.noreply.github.com>
2024-08-28 20:39:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function insert_Row() {
-    //Write your code here
-  
-  
+    var table = document.getElementById("sampleTable");
+    
+    var newRow = table.insertRow(0);
+    
+    var newCell1 = newRow.insertCell(0);
+    var newCell2 = newRow.insertCell(1);
+    
+    newCell1.textContent = "New Cell1";
+    newCell2.textContent = "New Cell2";
 }
+
